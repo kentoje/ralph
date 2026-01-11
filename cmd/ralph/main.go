0caf9b6f2254b0402fb0cb8e1e8eb2c62465fb82
Kento Monthubert <k.monthubert@gmail.com>
2026-01-11 21:11:31 +0100

    change gitignore

---
+++ cmd/ralph/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/kento/ralph/internal/commands"
+	"github.com/kento/ralph/internal/ui/format"
+	"github.com/kento/ralph/internal/ui/styles"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	// No args - show interactive command picker
+	if len(args) == 0 {
+		if err := commands.RunInteractivePicker(); err != nil {
+			fmt.Fprintln(os.Stderr, format.FormatError(err.Error()))
+			os.Exit(1)
+		}
+		return
+	}
+
+	cmd := args[0]
+	cmdArgs := args[1:]
+
+	var err error
+	switch cmd {
+	case "help", "--help", "-h":
+		fmt.Print(commands.HelpText)
+	case "setup":
+		err = commands.Setup()
+	case "home":
+		err = commands.Home()
+	case "init":
+		err = commands.Init()
+	case "run":
+		maxIterations := commands.DefaultMaxIterations
+		if len(cmdArgs) > 0 {
+			if n, parseErr := strconv.Atoi(cmdArgs[0]); parseErr == nil {
+				maxIterations = n
+			}
+		}
+		err = commands.Run(maxIterations)
+	case "status":
+		err = commands.Status()
+	case "prd":
+		err = commands.Prd()
+	case "list":
+		err = commands.List()
+	case "archive":
+		err = commands.Archive()
+	case "clean":
+		all := len(cmdArgs) > 0 && (cmdArgs[0] == "--all" || cmdArgs[0] == "-a")
+		err = commands.Clean(all)
+	default:
+		fmt.Fprintln(os.Stderr, styles.ErrorText.Render(fmt.Sprintf("Unknown command: %s", cmd)))
+		fmt.Println()
+		fmt.Println(styles.Muted.Render("Run 'ralph' for interactive mode or 'ralph help' for usage."))
+		os.Exit(1)
+	}
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, format.FormatError(err.Error()))
+		os.Exit(1)
+	}
+}
